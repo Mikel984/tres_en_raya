200589a683e82eaeec61fd0499550ed975f45888
mroca <rocamiguel84@gmail.com>
2023-09-24 22:50:03 +0200

    solution untill winner check

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-const squares = document.querySelectorAll(".square");
+const x = "X"
+const o = "◯"
+let shift = "player1"
+const squares = document.querySelectorAll(".square")
 const squaresArray = [...squares]
-//const array = Array.from(squares) es lo mismo
-let shift = 0;
-let board = [];
-
-//console.log(squaresArray)
+let winner = false;
 
 
-squares.forEach((element)=>{
-    element.addEventListener("click", (event)=>{
-        //console.log(shift % 2 == 0)
-        //console.log(shift % 2)
-        const square = event.target;
-        let color = (shift % 2 == 0)? "red": "blue";
-        (shift % 2 == 0)? square.style.backgroundColor = 'red': square.style.backgroundColor = 'blue';
-        //(shift % 2 == 0)? square.classList.add("redSquare"): square.classList.add("blueSquare"); - Termina superponiendose la ultima clase del css al sobreponerse, otra opcion seria sacar la clase antes de volver a insertarla pero qeuda mas limpio aplicando el estilo css dinamicamente
-        let squareIndex = squaresArray.indexOf(square);
-        shift++;
-        //console.log(board)
-        board[squareIndex] = color;
-        //console.log(board[squareIndex]);
-        //Validamos si ha ganado
-        //if(isWinner()) alert(`Bien jugado el jugador ${color} es el ganador!!!`);
-        let isWinner = false;
-        if(board[0] == board[1] && board[0] == board[2] && board[0]) isWinner = true;
-        if(board[3] == board[4] && board[3] == board[5] && board[3]) isWinner = true;
-        if(board[6] == board[7] && board[6] == board[8] && board[6]) isWinner = true;
-        if(board[0] == board[3] && board[0] == board[6] && board[0]) isWinner = true;
-        if(board[1] == board[4] && board[1] == board[7] && board[1]) isWinner = true;
-        if(board[2] == board[5] && board[2] == board[8] && board[2]) isWinner = true;
-        if(board[0] == board[4] && board[0] == board[8] && board[0]) isWinner = true;
-        if(board[2] == board[4] && board[2] == board[6] && board[2]) isWinner = true;
-        if(isWinner)alert(`Bien jugado el jugador ${color} es el ganador!!!`);
+// 1 asignamos evento a cada casilla y retornamos indice de cada una de elleas tambien
+squares.forEach((element,index)=>{
+    element.addEventListener("click",()=>{
+        // 2 rellenamos casillas con cada click
+        // cruz o raya segun el turno
+        (shift == "player1")? element.innerHTML = x: element.innerHTML = o;
+        //cambiamos turno cada vez que hacemos click
+        (shift == "player1")? shift = "player2": shift = "player1";
+        // 3 revisar ganador
+        //creamos array a partir de los elemento de nuestra array7collection inicial
+        // comprobamos si hay ganador en filas
+        const gameArray = squaresArray.map(element=>element.textContent) 
+        let combination = [];
+        for(let i= 0; i<=9;i+=3){
+            if(gameArray[i] == gameArray[i+1] && gameArray[i] == gameArray[i+2] && gameArray[i]){
+                winner==true
+                combination = [i,i+1,i+2]
+                console.log (`Enhorabuena ganaste con la combinacion ${combination}`)
+            }
+        }
+        for(let i= 0; i<=3;i++){
+            if(gameArray[i] == gameArray[i+3] && gameArray[i] == gameArray[i+6] && gameArray[i]){
+                combination = [i,i+3,i+6]
+                console.log (`Enhorabuena ganaste con la combinacion ${combination}`)
+            }
+        }
+        if(gameArray[0] == gameArray[4] && gameArray[0] == gameArray[8] && gameArray[0]){
+            combination = [0,4,8]
+            console.log (`Enhorabuena ganaste con la combinacion ${combination}`)
+        }
+        if(gameArray[2] == gameArray[4] && gameArray[2] == gameArray[6] && gameArray[2]){
+            combination = [2,4,6]
+            console.log (`Enhorabuena ganaste con la combinacion ${combination}`)
+        }
         })
-});
+    })  //* problema !!!!CONTINUA EJECUTANDO LA COMPROBACION UNA VEZ TENEMOS UN GANADOR.... */
+
+    
+    /*squaresArray.forEach((element,i)=>{   !!!!!!!!!!!!!!!!! conseguir sacar la matriz cada vez con un forEach en lugar de map!!!
+        const content = element.textContent;
+        squaresArray.splice(index,1,content);
+        console.log(squaresArray) 
+    })*/
+
+    
+    
